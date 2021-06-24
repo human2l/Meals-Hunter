09bb67109fb166b7f0e93861f7c750416acb78db
Kai <human2lkai@gmail.com>
2021-06-24 14:04:47 +1000

    add icon and splash screen

--- App.js
+++ App.js
@@ -52,3 +52,7 @@ export default function App() {
     </>
   );
 }
+
+{
+  /* <div>Icons made by <a href="https://www.flaticon.com/authors/surang" title="surang">surang</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div> */
+}
